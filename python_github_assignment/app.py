1b6b4a49e5e1da36bf4fbf6b893cad824beac9b1
Wenjie Li <wenjieli@usf.edu>
2025-11-23 22:07:22 -0500

    Add formatted output and display calculations

--- python_github_assignment/app.py
+++ python_github_assignment/app.py
@@ -2,3 +2,4 @@ print("Welcome to my python program!")
 hours = input("How many hours did you practice today?")
 hours = float(hours)
 weekly_hours = hours * 7
+print(f"You are on track to practice {weekly_hours} this week")
