8e901d86a064f69edc3fa8a2cbb49e14bb6a21d0
Wenjie Li <wenjieli@usf.edu>
2025-11-23 22:08:16 -0500

    Add basic error handling for user input

--- python_github_assignment/app.py
+++ python_github_assignment/app.py
@@ -3,3 +3,8 @@ hours = input("How many hours did you practice today?")
 hours = float(hours)
 weekly_hours = hours * 7
 print(f"You are on track to practice {weekly_hours} this week")
+try:
+    hours = float(hours)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
