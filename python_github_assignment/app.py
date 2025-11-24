405cfef947cf548b7b8ab7b439e76049716f1a07
Wenjie Li <wenjieli@usf.edu>
2025-11-23 22:10:28 -0500

    Clean up code and add comments

--- python_github_assignment/app.py
+++ python_github_assignment/app.py
@@ -1,8 +1,15 @@
+#welcome message and user input
 print("Welcome to my python program!")
 hours = input("How many hours did you practice today?")
+
+#data conversion and calculation
 hours = float(hours)
 weekly_hours = hours * 7
+
+#display results
 print(f"You are on track to practice {weekly_hours} this week")
+
+#error handling
 try:
     hours = float(hours)
 except ValueError:
